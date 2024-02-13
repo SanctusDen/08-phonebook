89b9222381e01fe1b66215a908f34b6b82284402
Dionisii <121027564+SanctusDen@users.noreply.github.com>
2024-02-13 19:18:09 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="goit-react-hw-08-phonebook">
+    <BrowserRouter basename="08-phonebook">
       <Provider store={store}>
         <PersistGate persistor={persistor}>
           <App />
